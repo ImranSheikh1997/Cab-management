323d349c96f0c332a14bad6151ac7e8238ae5d72
Imran Sheikh <imran.sheikh@globalvoxinc.com>
2021-12-03 23:37:54 +0530

    initial commit

---
+++ src/test/java/cabmanagement/example/cabmanagement/CabManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package cabmanagement.example.cabmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CabManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
